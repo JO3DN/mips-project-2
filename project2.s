e4dfc15ff47111605ee049c31645d28b1e8647ea
Joed Ngangmeni <39495858+JO3DN@users.noreply.github.com>
2018-11-12 18:15:46 -0500

    getting user input

--- project2.s
+++ project2.s
@@ -2,6 +2,11 @@
 emptyinput: .asciiz "Input is empty."
 inputoolong: .asciiz "Input is too long."
 invalidbase: .asciiz "Invalid base-33 number."
+userInput: .space 1000
 
 .text
 main: 
+# getting user input
+li $v0 , 8
+la $a0 , userInput
+
