d21f3f5de24973221f8787b9e670cb359f61f219
Joed Ngangmeni <39495858+JO3DN@users.noreply.github.com>
2018-11-12 20:53:21 -0500

    looking at the first bite
    vhecking if its a null string (empty)

--- project2.s
+++ project2.s
@@ -1,13 +1,17 @@
 .data
-emptyinput: .asciiz "Input is empty."
-inputoolong: .asciiz "Input is too long."
-invalidbase: .asciiz "Invalid base-33 number."
-userInput: .space 1000
+	emptyinput: .asciiz "Input is empty."
+	inputoolong: .asciiz "Input is too long."
+	invalidbase: .asciiz "Invalid base-33 number."
+	userInput: .space 1000
 
 .text
-main: 
-# getting user input
-li $v0 , 8
-la $a0 , userInput
-li $a1 , 1000
-syscall
+	main: 
+	# getting user input
+	li $v0 , 8
+	la $a0 , userInput
+	li $a1 , 1000
+	syscall
+	#making sure $t is empty (0) 
+	li $t0 , 0
+		Char:
+		lbu $t0 , 0($a0)
